0cc440c5f31e52e3d42f8bb71ea287e968c59b35
Gutscs <gutscs@gmail.com>
2022-05-30 20:13:31 -0300

    :hammer: Production env test

--- src/run_model_production.sh
+++ src/run_model_production.sh
@@ -2,7 +2,7 @@
 strDate=$(date +'%Y-%m-%dT%H-%M-%S')
 
 # path
-path_to_papermill='/c/Users/gutsc/anaconda3/envs/insiders_clustering/Scripts'
+path_to_papermill='/home/ubuntu/.pyenv/versions/3.8.12/envs/insiders_clustering/bin/'
 
 # run papermill
 $path_to_papermill/papermill models/Insiders_Clustering_deploy.ipynb reports/Insiders_Clustering_deploy_$strDate.ipynb
