1d34995a0beee6824a7daabfb0465b4274be301a
Gutscs <gutscs@gmail.com>
2022-06-01 13:50:16 -0300

    :memo: Update file

--- src/run_model_production.sh
+++ src/run_model_production.sh
@@ -2,7 +2,7 @@
 strDate=$(date +'%Y-%m-%dT%H-%M-%S')
 
 # path
-path_to_papermill='/home/ubuntu/.pyenv/versions/3.8.12/envs/insiders_clustering/bin/'
+path_to_papermill='/home/ubuntu/.pyenv/versions/3.8.12/envs/insiders_clustering/bin'
 
 # run papermill
 $path_to_papermill/papermill models/Insiders_Clustering_deploy.ipynb reports/Insiders_Clustering_deploy_$strDate.ipynb
